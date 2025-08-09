6b01e238363429908e9f2431081c1aac97acb03b
linzero <root@linli.eu.org>
2025-08-09 16:43:56 +0800

    fix: 更新下游应用端口，确保与 Caddyfile.template 一致

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 export HOME=/data
 
 # 设置下游应用端口
-DOWNSTREAM_PORT=${DOWNSTREAM_PORT:-8000}
+DOWNSTREAM_PORT=${DOWNSTREAM_PORT:-3002}
 
 # 生成 Caddyfile
-sed "s,{{.DOWNSTREAM_PORT | default \"8000\"}},$DOWNSTREAM_PORT," /data/Caddyfile.template > /tmp/Caddyfile
+sed "s,{{.DOWNSTREAM_PORT | default \"3002\"}},$DOWNSTREAM_PORT," /data/Caddyfile.template > /tmp/Caddyfile
 
 # 在后台静默启动主应用程序
 if [ -f "/app/curl" ]; then
